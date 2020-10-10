e2414e1fc424873934fe9af8f13a635f622e7530
IT Shatle <itshatle@iMac-Lomray.local>
2020-10-10 16:26:29 +0300

    “first_js”

--- index2.js
+++ index2.js
@@ -1,5 +1,4 @@
-alert('Hi, dud');
-function substruct(firstNum, secondNum) {
+const substract = function (firstNum, secondNum) {
     if (typeof firstNum !== "number" || typeof secondNum !== "number") {
         return "ERROR";
     }
@@ -8,4 +7,30 @@ function substruct(firstNum, secondNum) {
 
 
 
-console.log(substruct("9", 4))
+console.log(substract("9", 4))
+// const result=substruct(7,3) результат в переменную
+
+const substract2 = function (thirdNum, fourthNum) {
+    if (typeof thirdNum === "number" || typeof fourthNum === "number") {
+        return thirdNum - fourthNum;
+    }
+
+    return "ERROR";
+}
+
+
+
+console.log(substract2(9, 4))
+
+
+
+
+const sayHello = function (name) {
+    if (typeof name === "string") {
+        // return "Hello," + name;
+        return `Hello, ${name}`
+    }
+}
+const name = "Danik"
+
+alert(sayHello(name));
